ad97bd1bc610fdd0ad4f93fe99e52bfee91f2246
jcwoolridge <69435796+jcwoolridge@users.noreply.github.com>
2020-10-28 11:21:29 -0400

    Objects update v2

--- js/ratings.js
+++ js/ratings.js
@@ -1,11 +1,9 @@
-collect_ratings()
-let rating = {
-
-    count: 0,
-    sum: 0,
-    average: 0,
-    rating: 0,
+const collect_ratings = () => {
+    const ratings = {
+        'count': 0,
+        'sum': 0,
+        'average': 0,
+    };
 }
 
-
-querySelectorAll(rating)
+querySelectorAll(ratings)
