b2a72ed5612c31ee277c6b42a1a80033b75aed30
jcwoolridge <69435796+jcwoolridge@users.noreply.github.com>
2021-01-06 09:18:03 -0500

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -2,8 +2,7 @@ const collect_ratings = () => {
     const ratings = {
         'count': 0,
         'sum': 0,
-        'average': 0,
+        'average': 0
     };
-}
-
-querySelectorAll(ratings)
+    let rating = 0;
+};
