c9b2b13f4b064449804ec65c8236e0dea4b2eac2
jcwoolridge <69435796+jcwoolridge@users.noreply.github.com>
2021-06-22 19:17:34 -0400

    adjusted rating

--- js/ratings.js
+++ js/ratings.js
@@ -1,10 +1,11 @@
-const collect_ratings = () => {
-    const ratings = {
+const collect_ratings = () => {  
+const ratings = {
         count: 0,
         sum: 0,
-        average: 0
+        average: 0,
     };
-   rating = 0;
+   
+let rating = 0  
 const elements = document.querySelectorAll('.rating');
 
 elements.forEach(function(element) {
